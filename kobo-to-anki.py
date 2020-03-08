b4f028953a52be0284927cb3c4eeb081d0e22e8b
Sven van Heugten <svenvanheugten@home.nl>
2020-03-08 11:37:29 +0100

    Very simple retrieval of chapter text.

--- kobo-to-anki.py
+++ kobo-to-anki.py
@@ -1,10 +1,29 @@
 import sqlite3
+from zipfile import ZipFile
+
+
+VOLUME = '/Volumes/KOBOeReader/'
+
 
 if __name__ == '__main__':
-    db = sqlite3.connect('/Volumes/KOBOeReader/.kobo/KoboReader.sqlite')
+    db = sqlite3.connect(VOLUME + '.kobo/KoboReader.sqlite')
 
     cursor = db.cursor()
     cursor.execute('''SELECT ContentID, StartContainerPath, EndContainerPath, Text FROM Bookmark''')
 
     for content_id, start_container_path, end_container_path, text in cursor:
+        if not content_id.startswith('file:///mnt/onboard/'):
+            continue
+
         print(text)
+
+        book_file, suffix = content_id.split('#', 1)
+        _, xhtml_file = suffix.split(')', 1)
+
+        if '#' in xhtml_file:
+            xhtml_file = xhtml_file.split('#')[0]
+
+        with ZipFile(VOLUME + book_file[20:]) as book_zip:
+            with book_zip.open(xhtml_file) as book_chapter:
+                chapter_text = book_chapter.read()
+
